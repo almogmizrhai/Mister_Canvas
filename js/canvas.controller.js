8802eaa178ce423dc5e399b541b06911d5d31cb8
Almog Mizrahi <almogosh111@gmail.com>
2025-09-17 23:29:31 +0300

    Select background from user device

--- js/canvas.controller.js
+++ js/canvas.controller.js
@@ -41,8 +41,27 @@ function onDownloadCanvas(elLink) {
     elLink.download = 'my-img'
 }
 
-function onLoadCanvas() {
-    console.log('load canvas')
+function onImgInput(ev) {
+    loadImageFromInput(ev, renderImg)
+}
+
+function loadImageFromInput(ev, onImageReady) {
+    const reader = new FileReader()
+
+    reader.onload = (event) => {
+        const img = new Image()
+        img.src = event.target.result
+
+        img.onload = () => {
+            onImageReady(img)
+        }
+    }
+
+    reader.readAsDataURL(ev.target.files[0])
+}
+
+function renderImg(img) {
+    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
 }
 
 function onShareFacebook() {
